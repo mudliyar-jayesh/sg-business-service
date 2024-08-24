7aa6d8ba210f7f2c1eb33843694c0a32074d4e76
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-08-25 03:36:54 +0530

    Basic Group By Logic

--- endpoints/outstanding.go
+++ endpoints/outstanding.go
@@ -1,5 +1,6 @@
 package endpoints
 import (
+    "fmt"
     "encoding/json"
     "context"
     "net/http"
@@ -23,6 +24,31 @@ func GetOutstandingReport(res http.ResponseWriter, req *http.Request) {
     }
     var results handlers.DocumentResponse= mongoHandler.FindDocuments(docFilter)
 
+    groupedDocs := GroupByLedgerName(results.Data)
+
+    responseData, err := json.Marshal(groupedDocs)
+    if err != nil {
+        http.Error(res, "Error encoding response data", http.StatusInternalServerError)
+        return
+    }
+
     res.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(res).Encode(results.Data)
+    res.WriteHeader(http.StatusOK)
+    res.Write(responseData)
+
+}
+
+func GroupByLedgerName(documents []bson.M) map[string][]bson.M {
+    grouped := make(map[string][]bson.M)
+
+    for _, doc := range documents {
+        ledgerName, exists := doc["LedgerName"].(string)
+        if !exists {
+            fmt.Println("LedgerName field missing or not a string")
+            continue
+        }
+        grouped[ledgerName] = append(grouped[ledgerName], doc)
+    }
+
+    return grouped
 }
