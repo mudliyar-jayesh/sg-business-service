cb37f5666fa9fbada65904aee52596ea495cd0af
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-09-12 12:59:34 +0530

    Location Wise Bill Endpoint

--- modules/outstanding/helpers.go
+++ modules/outstanding/helpers.go
@@ -5,6 +5,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"sg-business-service/config"
 	"sg-business-service/handlers"
+	"sg-business-service/models"
 	"sg-business-service/utils"
 )
 
@@ -65,4 +66,56 @@ func GetOutstandingByFilter(mongoFilter bson.M, requestFilter OsReportFilter, us
 
 	var handler = GetOutstandingCollection()
 	return handler.FindDocuments(docFilter)
+}
+
+func GetBills(companyId string, requestFilter models.RequestFilter, isDebit bool, additionalFilter bson.M) []MetaBill {
+	var parentName string = "Current Assets"
+	if !isDebit {
+		parentName = "Current Liabilities"
+	}
+	var groups = handlers.CachedGroups.GetChildrenNames(companyId, parentName)
+
+	var filter = bson.M{
+		"CompanyId": companyId,
+		"LedgerGroupName": bson.M{
+			"$in": groups,
+		},
+		"ClosingBal":         bson.M{"$ne": nil},
+		"ClosingBal.IsDebit": isDebit,
+	}
+	if additionalFilter != nil {
+		filter["$and"] = additionalFilter
+	}
+
+	docFilter := handlers.DocumentFilter{
+		Ctx:           context.TODO(),
+		Filter:        filter,
+		UsePagination: requestFilter.Batch.Apply,
+		Limit:         requestFilter.Batch.Limit,
+		Offset:        requestFilter.Batch.Offset,
+		Projection: bson.M{
+			"LedgerName":      1,
+			"LedgerGroupName": 1,
+			"BillDate":        "$BillDate.Date",
+			"DueDate":         "$BillCreditPeriod.DueDate",
+			"Amount":          "$ClosingBal.Amount",
+			"OpeningAmount":   "$OpeningBal.Amount",
+			"Name":            "$Name",
+			"_id":             0,
+		},
+		Sorting: bson.D{
+			{
+				Key:   getFieldBySortKey(requestFilter.SortKey),
+				Value: utils.GetValueBySortOrder(requestFilter.SortOrder),
+			},
+		},
+	}
+
+	var handler = GetOutstandingCollection()
+	bills, err := handlers.GetDocuments[MetaBill](handler, docFilter)
+	if err != nil {
+		return make([]MetaBill, 0)
+	}
+	return bills
+
 }
