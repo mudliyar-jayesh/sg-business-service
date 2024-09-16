c04ceba9b8f7249074698ad83574bbfc23470993
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-09-16 21:25:57 +0530

    Email sending tested

--- endpoints/outstanding_settings.go
+++ endpoints/outstanding_settings.go
@@ -13,133 +13,136 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-
 func CreateOsSetting(res http.ResponseWriter, req *http.Request) {
-    collection := handlers.GetCollection("BMRM", "OutstandingSettings")
-    var mongoHandler = handlers.NewMongoHandler(collection)
+	collection := handlers.GetCollection("BMRM", "OutstandingSettings")
+	var mongoHandler = handlers.NewMongoHandler(collection)
 
-    companyId := req.Header.Get("CompanyId")
+	companyId := req.Header.Get("CompanyId")
 
-    body, err := utils.ReadRequestBody[outstanding.OsShareSettings](req)
-    if err != nil {
-        http.Error(res, "Unable to read request body", http.StatusBadRequest)
-        return
-    }
+	body, err := utils.ReadRequestBody[outstanding.OsShareSettings](req)
+	if err != nil {
+		http.Error(res, "Unable to read request body", http.StatusBadRequest)
+		return
+	}
 
-    body.CompanyId = companyId
+	body.CompanyId = companyId
 
-    docFilter := handlers.DocumentFilter {
-        Ctx: context.TODO(),
-        Filter: bson.M {
-            "CompanyId": companyId,
-        },
-        UsePagination: false,
-        Limit: 0,
-        Offset: 0,
-    }
+	docFilter := handlers.DocumentFilter{
+		Ctx: context.TODO(),
+		Filter: bson.M{
+			"CompanyId": companyId,
+		},
+		UsePagination: false,
+		Limit:         0,
+		Offset:        0,
+	}
 
-    var results handlers.DocumentResponse= mongoHandler.FindDocuments(docFilter)
-    fmt.Println(results.Data)
-    if len(results.Data) != 0 {
-        http.Error(res, "Already Exists", http.StatusBadRequest)
-        return;
-    }
+	var results handlers.DocumentResponse = mongoHandler.FindDocuments(docFilter)
+	fmt.Println(results.Data)
+	if len(results.Data) != 0 {
+		http.Error(res, "Already Exists", http.StatusBadRequest)
+		return
+	}
 
-    _, err = handlers.InsertDocument("BMRM", "OutstandingSettings", body)
-    if err != nil {
-        http.Error(res, "Could not create entry", http.StatusBadRequest)
-        return
-    }
-    res.WriteHeader(http.StatusOK)
+	_, err = handlers.InsertDocument("BMRM", "OutstandingSettings", body)
+	if err != nil {
+		http.Error(res, "Could not create entry", http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
 
 func UpdateOsSetting(res http.ResponseWriter, req *http.Request) {
-    collection := handlers.GetCollection("BMRM", "OutstandingSettings")
-    var mongoHandler = handlers.NewMongoHandler(collection)
+	collection := handlers.GetCollection("BMRM", "OutstandingSettings")
+	var mongoHandler = handlers.NewMongoHandler(collection)
 
-    companyId := req.Header.Get("CompanyId")
+	companyId := req.Header.Get("CompanyId")
 
-    body, err := utils.ReadRequestBody[outstanding.OsShareSettings](req)
-    if err != nil {
-        http.Error(res, "Unable to read request body", http.StatusBadRequest)
-        return
-    }
+	body, err := utils.ReadRequestBody[outstanding.OsShareSettings](req)
+	if err != nil {
+		http.Error(res, "Unable to read request body", http.StatusBadRequest)
+		return
+	}
 
-    body.CompanyId = companyId
+	body.CompanyId = companyId
 
-    filter := bson.M {
-        "CompanyId": companyId,
-    }
+	filter := bson.M{
+		"CompanyId": companyId,
+	}
 
-    update := bson.M {
-        "$set": body,
-    }
+	update := bson.M{
+		"$set": body,
+	}
 
-    mongoHandler.UpdateDocument("BMRM", "OutstandingSettings", filter, update)
-    if err != nil {
-        http.Error(res, "Could not create entry", http.StatusBadRequest)
-        return
-    }
-    res.WriteHeader(http.StatusOK)
+	mongoHandler.UpdateDocument("BMRM", "OutstandingSettings", filter, update)
+	if err != nil {
+		http.Error(res, "Could not create entry", http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
 
 func GetSetting(res http.ResponseWriter, req *http.Request) {
-    collection := handlers.GetCollection("BMRM", "OutstandingSettings")
-    var mongoHandler = handlers.NewMongoHandler(collection)
+	collection := handlers.GetCollection("BMRM", "OutstandingSettings")
+	var mongoHandler = handlers.NewMongoHandler(collection)
 
-    companyId := req.Header.Get("CompanyId")
+	companyId := req.Header.Get("CompanyId")
 
-    docFilter := handlers.DocumentFilter {
-        Ctx: context.TODO(),
-        Filter: bson.M {
-            "CompanyId": companyId,
-        },
-        UsePagination: false,
-        Limit: 0,
-        Offset: 0,
-    }
+	docFilter := handlers.DocumentFilter{
+		Ctx: context.TODO(),
+		Filter: bson.M{
+			"CompanyId": companyId,
+		},
+		UsePagination: false,
+		Limit:         0,
+		Offset:        0,
+	}
 
-    var results handlers.DocumentResponse= mongoHandler.FindDocuments(docFilter)
-    if results.Err != nil {
-        http.Error(res, "No Data", http.StatusBadRequest)
-        return;
-    }
-    response := utils.NewResponseStruct(results.Data, len(results.Data))
-    response.ToJson(res);
+	var results handlers.DocumentResponse = mongoHandler.FindDocuments(docFilter)
+	if results.Err != nil {
+		http.Error(res, "No Data", http.StatusBadRequest)
+		return
+	}
+	response := utils.NewResponseStruct(results.Data, len(results.Data))
+	response.ToJson(res)
 
 }
 
 func SendEmail(res http.ResponseWriter, req *http.Request) {
 
-    to := make([]string, 1)
-    to[0] = "softgen.aquib.shaikh@gmail.com"
-    cc := make([]string, 1)
-    cc[0] = "jayeshmudlyiar2112000@gmail.com"
+	to := make([]string, 1)
+	to[0] = "softgen.aquib.shaikh@gmail.com"
+	cc := make([]string, 1)
+	cc[0] = "jayeshmudlyiar2112000@gmail.com"
 
-    var emailSettings = models.EmailSettings {
-      To: to,
-      Cc: cc,
-      SmtpPort:"587",
-      SmtpServer: "smtp.gmail.com",
-      Subject: "Sample Email",
-      Body: "Here is a sample email",
-      BodyType: 1,
-    }
-    err := handlers.SendEmail(emailSettings)
-    if err != nil  {
-        fmt.Println("Failed to send email:", err)
-        http.Error(res, "Could not send email", http.StatusBadRequest)
-    }
+	var emailSettings = models.EmailSettings{
+		To:         to,
+		Cc:         cc,
+		SmtpPort:   "587",
+		SmtpServer: "smtp.gmail.com",
+		Subject:    "Sample Email",
+		Body:       "Here is a sample email",
+		BodyType:   1,
+	}
+	err := handlers.SendEmail(emailSettings)
+	if err != nil {
+		fmt.Println("Failed to send email:", err)
+		http.Error(res, "Could not send email", http.StatusBadRequest)
+	}
 }
 
 func SendLedgerEmail(res http.ResponseWriter, req *http.Request) {
-    companyId := req.Header.Get("CompanyId")
+	companyId := req.Header.Get("CompanyId")
 
-    parties, err := utils.ReadRequestBody[[]string](req)
+	parties, err := utils.ReadRequestBody[EmailRequest](req)
 
-    if (err != nil) {
-        fmt.Println("Error parsing the list of ledgerNames")
-    }
+	if err != nil {
+		fmt.Println("Error parsing the list of ledgerNames")
+	}
 
-    reminders.SendEmailReminder(companyId, *parties)
+	reminders.SendEmailReminder(companyId, parties.Parties)
+}
+
+type EmailRequest struct {
+	Parties []string
 }
--- modules/outstanding/reminders/reports.go
+++ modules/outstanding/reminders/reports.go
@@ -16,141 +16,142 @@ import (
 )
 
 func SendEmailReminder(companyId string, ledgerNames []string) {
-    ledgers := ledgersMod.GetLedgerByNames(companyId, ledgerNames)
-    ledgerByName, err := utils.ToDictionary(ledgers, "Name")
-    if err != nil {
-        return
-    }
+	ledgers := ledgersMod.GetLedgerByNames(companyId, ledgerNames)
+	ledgerByName, err := utils.ToDictionary(ledgers, "Name")
+	if err != nil {
+		return
+	}
 
-    // Get Outstanding settings from Database for this company
-    settingData, settingErr := configMod.GetAllSettings(companyId)
-    if settingData == nil || len(settingData) > 1 {
-        fmt.Println(settingErr)
-        return
-    }
-    setting := settingData[0]
+	// Get Outstanding settings from Database for this company
+	settingData, settingErr := configMod.GetAllSettings(companyId)
+	if settingData == nil || len(settingData) > 1 {
+		fmt.Println(settingErr)
+		return
+	}
+	setting := settingData[0]
 
-    for key, _ := range ledgerByName {
+	for key, _ := range ledgerByName {
 
-        collectionFilter := bson.M {
-            "CompanyId": companyId,
-            "LedgerName": key,
-        }
+		collectionFilter := bson.M{
+			"CompanyId":  companyId,
+			"LedgerName": key,
+		}
 
-        // if cutoff date is not present then apply billDate filter.
-        if len(setting.CutOffDate) > 0 {
-            parsedDate, err := time.Parse("2006-01-02", setting.CutOffDate)
+		// if cutoff date is not present then apply billDate filter.
+		if len(setting.CutOffDate) > 0 {
+			parsedDate, err := time.Parse("2006-01-02", setting.CutOffDate)
 
-            if err != nil {
-                fmt.Println("Error occoured while parsing date", setting.CutOffDate)
-            }
+			if err != nil {
+				fmt.Println("Error occoured while parsing date", setting.CutOffDate)
+			}
 
-            collectionFilter = bson.M {
-            "CompanyId": companyId,
-            "LedgerName": key,
-            "BillDate.Date": bson.M{
-                "$gte":parsedDate,
-            },
-        }
-        }
+			collectionFilter = bson.M{
+				"CompanyId":  companyId,
+				"LedgerName": key,
+				"BillDate.Date": bson.M{
+					"$gte": parsedDate,
+				},
+			}
+		}
 
-        dbFilter := handlers.DocumentFilter {
-            Ctx: context.TODO(),
-            Filter: collectionFilter,
-            UsePagination: false,
-            Projection: bson.M{
-                "LedgerName": 1,
-                "LedgerGroupName": 1,
-                "BillDate": "$BillDate.Date",
-                "DueDate": "$BillCreditPeriod.DueDate",
-                "Amount": "$ClosingBal.Amount",
-                "OpeningAmount": "$OpeningBal.Amount",
-                "Name": "$Name",
-                "_id": 0,
-            },
-        }
-        billResponse := osMod.GetOutstandingCollection().FindDocuments(dbFilter)
-        if billResponse.Err != nil {
-            continue
-        }
-        var bills []osMod.Bill
-        var totalAmount float64 = 0
-        istLocation, _ := time.LoadLocation("Asia/Kolkata")
-        for _, item := range billResponse.Data {
-            billDateValue := item["BillDate"].(primitive.DateTime).Time()
-            billDate := billDateValue.In(istLocation).Format("2006-01-02 15:04:05")
+		dbFilter := handlers.DocumentFilter{
+			Ctx:           context.TODO(),
+			Filter:        collectionFilter,
+			UsePagination: false,
+			Projection: bson.M{
+				"LedgerName":      1,
+				"LedgerGroupName": 1,
+				"BillDate":        "$BillDate.Date",
+				"DueDate":         "$BillCreditPeriod.DueDate",
+				"Amount":          "$ClosingBal.Amount",
+				"OpeningAmount":   "$OpeningBal.Amount",
+				"Name":            "$Name",
+				"_id":             0,
+			},
+		}
+		billResponse := osMod.GetOutstandingCollection().FindDocuments(dbFilter)
+		if billResponse.Err != nil {
+			continue
+		}
+		var bills []osMod.Bill
+		var totalAmount float64 = 0
+		istLocation, _ := time.LoadLocation("Asia/Kolkata")
+		for _, item := range billResponse.Data {
+			billDateValue := item["BillDate"].(primitive.DateTime).Time()
+			billDate := billDateValue.In(istLocation).Format("2006-01-02 15:04:05")
 
-            var dueDate string
-            dueDateValue := item["DueDate"]
-            if dueDateValue == nil {
-                dueDate = billDate
-            } else {
-                dueDate = dueDateValue.(primitive.DateTime).Time().In(istLocation).Format("2006-01-02 15:04:05")
-            }
+			var dueDate string
+			dueDateValue := item["DueDate"]
+			if dueDateValue == nil {
+				dueDate = billDate
+			} else {
+				dueDate = dueDateValue.(primitive.DateTime).Time().In(istLocation).Format("2006-01-02 15:04:05")
+			}
 
-            layout := "2006-01-02 15:04:05"
-            parsedTime, _:= time.Parse(layout, dueDate)
-            today := time.Now().UTC()
+			layout := "2006-01-02 15:04:05"
+			parsedTime, _ := time.Parse(layout, dueDate)
+			today := time.Now().UTC()
 
-            // Calculate the difference
-            diff := today.Sub(parsedTime)
+			// Calculate the difference
+			diff := today.Sub(parsedTime)
 
-            // Get the difference in days
-            days := int32(diff.Hours() / 24)
+			// Get the difference in days
+			days := int32(diff.Hours() / 24)
 
-            // Additional filters
-            eligible := false;
+			// Additional filters
+			eligible := false
 
-            if ((setting.SendAllDue) ||
-                (setting.SendDueOnly &&  days < int32(setting.OverDueDays)) || 
-                (setting.SendOverDueOnly  && days >= int32(setting.OverDueDays))){
-                    eligible = true 
-            }
+			if (setting.SendAllDue) ||
+				(setting.SendDueOnly && days < int32(setting.OverDueDays)) ||
+				(setting.SendOverDueOnly && days >= int32(setting.OverDueDays)) {
+				eligible = true
+			}
 
-            if !eligible {
-                continue
-            }
+			if !eligible {
+				continue
+			}
 
-            var bill osMod.Bill = osMod.Bill {
-                LedgerName: item["LedgerName"].(string),
-                LedgerGroupName: item["LedgerGroupName"].(string),
-                BillName: item["Name"].(string),
-                DueDate: dueDate,
-                BillDate: billDate,
-                DelayDays: days,
-            }
+			var bill osMod.Bill = osMod.Bill{
+				LedgerName:      item["LedgerName"].(string),
+				LedgerGroupName: item["LedgerGroupName"].(string),
+				BillName:        item["Name"].(string),
+				DueDate:         dueDate,
+				BillDate:        billDate,
+				DelayDays:       days,
+			}
 
-            var amount = utils.ParseFloat64(item["Amount"])
-            bill.Amount = amount
-            totalAmount += amount
-            bills = append(bills, bill)
-        }
+			var amount = utils.ParseFloat64(item["Amount"])
+			bill.Amount = amount
+			totalAmount += amount
+			bills = append(bills, bill)
+		}
 
-        content := ReminderBody {
-            PartyName : key,
-            Address: "",
-            TotalAmount: totalAmount,
-            Bills: bills,
-        }
-        emailBody := handlers.WriteToTemplate("C:\\Users\\softg\\Projects\\sg-business-service\\osTemplate.html", content)
-        emailSetting := setting.EmailSetting
+		content := ReminderBody{
+			PartyName:   key,
+			Address:     "",
+			TotalAmount: totalAmount,
+			Bills:       bills,
+		}
+		//emailBody := handlers.WriteToTemplate("C:\\Users\\softg\\Projects\\sg-business-service\\osTemplate.html", content)
+		emailBody := handlers.WriteToTemplate("/home/jayesh/development/Softgen/sg-business-service/osTemplate.html", content)
+		emailSetting := setting.EmailSetting
 
-        var emailSettings = models.EmailSettings {
-            To: emailSetting.To,
-            Cc: emailSetting.Cc,
-            SmtpPort: emailSetting.SmtpPort,
-            SmtpServer: emailSetting.SmtpServer,
-            Subject: emailSetting.Subject,
-            Body: emailSetting.Body + emailBody,
-            BodyType: 1,
-        }
+		var emailSettings = models.EmailSettings{
+			To:         emailSetting.To,
+			Cc:         emailSetting.Cc,
+			SmtpPort:   emailSetting.SmtpPort,
+			SmtpServer: emailSetting.SmtpServer,
+			Subject:    emailSetting.Subject,
+			Body:       emailSetting.Body + emailBody,
+			BodyType:   1,
+		}
 
-        fmt.Printf("settings: \n",emailSettings.To)
-        // send email 
-        err := handlers.SendEmail(emailSettings)
-        if err != nil  {
-            fmt.Println("Failed to send email:", err)
-        }
-    }
+		fmt.Printf("settings: \n", emailSettings.To)
+		// send email
+		err := handlers.SendEmail(emailSettings)
+		if err != nil {
+			fmt.Println("Failed to send email:", err)
+		}
+	}
 
 }
